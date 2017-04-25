0ee1517d27a3467b16952200f5c389e97ed722ad
bon_lemon <you@example.com>
2017-04-26 01:06:38 +0300

    complite exemples

---
+++ blocks/examples.js
@@ -0,0 +1,24 @@
+/**
+ * Inialize block foto default value
+ */
+$(".b-example-block__foto").ready(function(){
+    $.each($(".b-example-block__foto"), function(){
+        $imgUrl = $( this ).find(':first-child').attr('href');
+        $(this).css('background-image', 'url(' + $imgUrl + ')');
+    })
+});
+
+/**
+ * Handle mouseover and mouseout event.
+ * mouseover - change background depending on the href
+ * mouseout - reset to first child href
+ */
+$(".b-example-block__foto-cell")
+  .mouseover(function() {
+    $imgUrl = $(this).attr('href');
+    $(this).parent().css('background-image', 'url(' + $imgUrl + ')');
+  })
+  .mouseout(function() {
+    $imgUrl = $( this ).parent().children().attr('href');
+    $(this).parent().css('background-image', 'url(' + $imgUrl + ')');
+  });
