6c8fe960d4ddf6aca62114b10385eddaf890b243
d.avilov <dimon009@list.ru>
2017-04-27 15:33:24 +0300

    add jquery tabs

---
+++ blocks/b-tab.js
@@ -0,0 +1,17 @@
+// Checking whether document is ready
+$(document).ready(function() {
+    
+    // Handle click on link
+    $(".b-tab__link").on('click', function(event){
+        var linkUrl = $(this).children('a').attr('href');
+      
+        // Show/Hide tabs
+        // .siblings() - находит соседей элемента и скрывает их
+        $(".b-tab " + linkUrl).show().siblings().hide();
+
+        // Change current tab to active
+        $(this).addClass('b-tab__link_active').siblings().removeClass('b-tab__link_active');
+
+        event.preventDefault();
+    })
+});
